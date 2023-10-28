4419ce586264e9df6e24b836bd98434981fd9e06
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-10-28 20:38:06 +0600

    Create Top bar

--- app/src/main/java/com/yhr/jfj/movieapp/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.yhr.jfj.movieapp
 
+import android.annotation.SuppressLint
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yhr.jfj.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +29,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                MainContent()
+                Column(modifier = Modifier.padding(top = 70.dp)) {
+                    MainContent()
+                }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyApp(content: @Composable () -> Unit){
+fun MyApp(content: @Composable () -> Unit) {
     MovieAppTheme {
-        content()
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.background
+                    ),
+                    title = { Text(text = "Movie App") })
+            }
+        ) {
+            content()
+        }
     }
 }
 
 @Composable
-fun MainContent(){
-    Surface(color = MaterialTheme.colorScheme.background
+fun MainContent() {
+    Surface(
+        color = MaterialTheme.colorScheme.background
     ) {
         Text(text = "YHRJFJ")
     }
