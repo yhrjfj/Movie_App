6529c4b976427e0a21be09c0482a646f508903bd
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-10-29 13:40:19 +0600

    Create LazyColumn and Card view

--- app/src/main/java/com/yhr/jfj/movieapp/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/MainActivity.kt
@@ -3,13 +3,30 @@ package com.yhr.jfj.movieapp
 import android.annotation.SuppressLint
 import android.icu.text.CaseMap.Title
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -18,8 +35,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yhr.jfj.movieapp.ui.theme.MovieAppTheme
@@ -29,7 +48,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Column(modifier = Modifier.padding(top = 70.dp)) {
+                Column(modifier = Modifier.padding(top = 70.dp, start = 8.dp, end = 8.dp)) {
                     MainContent()
                 }
             }
@@ -45,11 +64,12 @@ fun MyApp(content: @Composable () -> Unit) {
         Scaffold(
             topBar = {
                 TopAppBar(
-                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
                         containerColor = MaterialTheme.colorScheme.primary,
                         titleContentColor = MaterialTheme.colorScheme.background
                     ),
-                    title = { Text(text = "Movie App") })
+                    title = { Text(text = "Anime & Movie") },
+                )
             }
         ) {
             content()
@@ -58,11 +78,55 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(
-        color = MaterialTheme.colorScheme.background
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Attack On Titan",
+        " One Piece",
+        "Death Note",
+        "Dragon Ball Z",
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life Of Pie"
+    )
+) {
+    Column {
+        LazyColumn() {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+// MovieRow
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        elevation = CardDefaults.cardElevation(5.dp)
     ) {
-        Text(text = "YHRJFJ")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+                shadowElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
     }
 }
 
