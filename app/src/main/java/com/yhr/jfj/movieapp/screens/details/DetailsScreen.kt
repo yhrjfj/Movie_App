de349b794533ad4176973e02d76ef935ffb774b1
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-10-31 20:23:23 +0600

    Navigating for go back screen

--- app/src/main/java/com/yhr/jfj/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/screens/details/DetailsScreen.kt
@@ -2,14 +2,18 @@ package com.yhr.jfj.movieapp.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -22,6 +26,13 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
             verticalArrangement = Arrangement.Center,
         ) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.displayLarge)
+            Spacer(modifier = Modifier.height(25.dp))
+            // Button for go back to previous screen
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
