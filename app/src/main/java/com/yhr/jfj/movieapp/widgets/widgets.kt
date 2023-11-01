044aa341753863b931f068b9ed7fee989fc671e6
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-01 21:43:55 +0600

    Show poster with in full scale

--- app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.yhr.jfj.movieapp.widgets
 
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -24,10 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.yhr.jfj.movieapp.model.Movie
 import com.yhr.jfj.movieapp.model.getMovies
 
@@ -62,6 +65,7 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
             ) {
                 Image(
                     painter = rememberAsyncImagePainter(model = movie.images[0]),
+                    contentScale = ContentScale.FillBounds,
                     contentDescription = "Movie Poster"
                 )
 //                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
