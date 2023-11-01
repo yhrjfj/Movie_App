ffcf1f92158bb094e356cce92b3d953a3112704a
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-01 18:37:00 +0600

    Show movie name, director name and release year

--- app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
@@ -3,6 +3,7 @@ package com.yhr.jfj.movieapp.widgets
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -56,7 +58,11 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
             ) {
                 Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.headlineSmall)
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.labelMedium)
+                Text(text = "Release: ${movie.year}", style = MaterialTheme.typography.labelMedium)
+            }
         }
     }
 }
