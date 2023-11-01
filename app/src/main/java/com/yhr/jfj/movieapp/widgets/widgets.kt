1657261bf3c971ed48059191dd7f0cfdc115966e
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-01 22:45:26 +0600

    Add expanded column

--- app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/yhr/jfj/movieapp/widgets/widgets.kt
@@ -1,6 +1,7 @@
 package com.yhr.jfj.movieapp.widgets
 
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -15,6 +16,9 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -22,12 +26,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
@@ -38,11 +51,17 @@ import com.yhr.jfj.movieapp.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    // State for expend the card when user click down arrow icon
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -79,6 +98,31 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                     style = MaterialTheme.typography.labelMedium
                 )
                 Text(text = "Release: ${movie.year}", style = MaterialTheme.typography.labelMedium)
+                // Expended column
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(Color.DarkGray, fontSize = 13.sp)){
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Bold)){
+                                append(movie.plot)
+                            }
+                        })
+                    }
+                }
+
+                // Add down arrow. When user click that icon that card will be expended
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
             }
         }
     }
